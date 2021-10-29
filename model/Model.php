d728e96d0a58f55aa99ba50c7fe4970b686a2b4f
John <jonathan.besse@etu.umontpellier.fr>
2021-10-29 11:21:45 +0200

    On a un truc qui fonctionne

--- model/Model.php
+++ model/Model.php
@@ -1,5 +1,4 @@
 <?php
-            require_once '../lib/File.php';
             require_once File::build_path(array("config","Conf.php"));
             class Model {
                
@@ -15,8 +14,7 @@
                   // Connexion à la base de données            
                   // Le dernier argument sert à ce que toutes les chaines de caractères 
                   // en entrée et sortie de MySql soit dans le codage UTF-8
-                  self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,
-                                       array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+                  self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password);
                   // On active le mode d'affichage des erreurs, et le lancement d'exception en cas d'erreur
                   self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 }catch (PDOException $e) {
--- model/ModelVoiture.php
+++ model/ModelVoiture.php
@@ -1,5 +1,6 @@
 
         <?php
+			require_once 'lib/File.php';
         	require_once File::build_path(array("config","Conf.php"));
 		    require_once File::build_path(array("model","Model.php"));
             class ModelVoiture {
