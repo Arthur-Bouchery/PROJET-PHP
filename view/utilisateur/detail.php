52cf1250d2092fdfd0e4b8275758b3d14aeff8f2
Jonathan-Besse <jonathan.besse@etu.umontpellier.fr>
2021-11-26 09:50:51 +0100

    TEST

--- view/utilisateur/detail.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo ('Login:'.htmlspecialchars($u->get('login')).'<br>');
-    echo ('Nom: '.htmlspecialchars(' '.$u->get('nom')).'<br>');
-    echo ('prenom: '.htmlspecialchars(' '.$u->get('prenom')).'<br>');
-    echo ('<a href="?action=delete&&controller=utilisateur&&login='.$u->get('login').'"">supprimer</a>'.'<br>');
-    echo ('<a href="?action=update&&controller=utilisateur&&login='.$u->get('login').'"">Mettre a jour</a>'.'<br>');
-    echo '<br>';
-?>
--- view/utilisateur/list.php
+++
@@ -1,6 +0,0 @@
-<body>
-    <?php
-    foreach ($tab_u as $u)
-            echo ('<p> Utilisateur de login: ' . htmlspecialchars($u->getlogin()) . ' <a href=?action=read&&controller=utilisateur&&login='.$u->getlogin().'>Details</a> </p>');
-    ?>
-</body>
