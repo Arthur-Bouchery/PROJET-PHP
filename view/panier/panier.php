ea15dc6669b41608a374bba6dbf46202b173ae35
Jonathan-Besse <jonathan.besse@etu.umontpellier.fr>
2021-12-04 21:00:28 +0100

    Le panier qui se set
    La panier tah l'époque de nos grands frères

--- view/panier/panier.php
+++ view/panier/panier.php
@@ -1,4 +1,17 @@
 <?php
-foreach ($tab_panier as $k => $v)
-echo '<p> Replique d\'id ' . htmlspecialchars($k) . " de quantité ".htmlspecialchars($v)." <a href=?controller=replique&action=read&idReplique=".$k.">Details</a> <a href=?controller=panier&action=set&idReplique=$k&qte=0>Supprimer</a> </p>";
+foreach ($tab_panier as $k => $v) {
+    ?>
+    <p> Replique d'ID <?php echo htmlspecialchars($k); ?> de quantité <?php echo htmlspecialchars($v); ?> </p>
+    <a href="?controller=replique&action=read&idReplique=<?php echo $k ?>">Detail</a>
+    <a href="?controlelr=panier&action=set&idReplique=<?php echo $k; ?>&qte=0">Supprimer</a>
+    <form method="GET" action="index.php">
+        <input type="hidden" name="action" value="set" />
+        <input type="hidden" name="controller" value="panier" />
+        <input type="hidden" name="idReplique" value="<?php echo $k; ?>" />
+        <input type="number" name="qte" value="<?php echo $v; ?>" />
+        <input type="submit" value="Mettre à jour" />
+    </form>
+    <hr>
+    <?php
+}
 ?>
