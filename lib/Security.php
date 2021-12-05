7bdfa6f246b9881e02829310c3c9782145b51d8d
Arthur-Bouchery <arthur.bouchery@laposte.net>
2021-12-05 10:36:25 +0100

    salting the passwords

--- lib/Security.php
+++ lib/Security.php
@@ -2,8 +2,10 @@
 
 class Security
 {
+    private static $seed = 'MdpDeG@merz:';
     public static function hacher($texte_en_clair) {
-        $texte_hache = hash('sha256', $texte_en_clair);
+        //assaisonnement  du mdp
+        $texte_hache = hash('sha256', ''.static::$seed.$texte_en_clair);
         return $texte_hache;
     }
 
