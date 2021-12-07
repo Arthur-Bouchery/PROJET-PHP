795e182e478657a9fb21edf0e2008febf5b2af91
louis-marceron <72874947+louis-marceron@users.noreply.github.com>
2021-12-07 14:44:53 +0100

    CSS sur la vue générique

--- view/view.php
+++ view/view.php
@@ -3,33 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo $pagetitle; ?></title>
+    <link rel="stylesheet" type="text/css" href="view/style.css">
 </head>
+<body>
 <header>
-    <p style="border: 1px solid black;text-align:center;padding-right:1em;">
-        <a href="index.php?action=readAll&controller=repliques">Repliques</a>
-        <a href="index.php?&controller=clients&action=home"> <?php if(isset($_SESSION['prenomClient'])) {echo $_SESSION['prenomClient'];}
-            else {echo "Profil";}?></a>
-        <a href="view/preferences.html">Préférences</a>
-        <a href="index.php?action=readAll&controller=panier">Panier</a>
-        <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) { ?>
+    <a href="index.php?action=readAll&controller=repliques">Repliques</a>
+    <a href="index.php?&controller=clients&action=home"> <?php if (isset($_SESSION['prenomClient'])) {
+            echo $_SESSION['prenomClient'];
+        } else {
+            echo "Profil";
+        } ?></a>
+    <a href="view/preferences.html">Préférences</a>
+    <a href="index.php?action=readAll&controller=panier">Panier</a>
+    <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) { ?>
         <br>Pannel administrateur :
         <a href="index.php?action=readAll&controller=clients">Gestion des clients</a>
         <a href="index.php?action=create&controller=clients">Créer un client</a>
         <a href="index.php?action=create&controller=repliques">Créer une réplique</a>
-        <?php } ?>
-    </p>
+    <?php } ?>
 </header>
-<body>
-<?php
-// Si $controleur='voiture' et $view='list',
-// alors $filepath="/chemin_du_site/view/voiture/list.php"
-$filepath = File::build_path(array("view", static::$object, "$view.php"));
-require $filepath;
-?>
-</body>
+<main>
+    <?php
+    // Si $controleur='voiture' et $view='list',
+    // alors $filepath="/chemin_du_site/view/voiture/list.php"
+    $filepath = File::build_path(array("view", static::$object, "$view.php"));
+    require $filepath;
+    ?>
+</main>
 <footer>
-    <p style="border: 1px solid black;text-align:center;padding-right:1em;">
-        Site de vente de Repliques d'armes à feu (pour petit enfant)
-    </p>
+    Site de vente de Repliques d'armes à feu (pour petit enfant)
 </footer>
+</body>
 </html>
