ecea92666f86d588ad62c7c7cf52cb97c651dcb7
Jonathan-Besse <78204251+Jonathan-Besse@users.noreply.github.com>
2021-12-07 12:26:44 +0100

    Petit sessionnage v2

--- view/Repliques/list.php
+++ view/Repliques/list.php
@@ -3,5 +3,12 @@ foreach ($tab as $t) {
     $primary = $t->get('idReplique');
     $htmlPrimary = htmlspecialchars($primary);
     $urlPrimary = rawurlencode($primary);
-    echo '<p><a href="index.php?controller=repliques&action=delete&idReplique=' . $urlPrimary . '">X</a> Réplique d\'identifiant <a href="index.php?controller=repliques&action=read&idReplique=' . $urlPrimary . '">' . $htmlPrimary . '</a> </p>';
-}
+    ?>
+    <?php if(isset($_SESSION['admin']) && $_SESSION['admin'] == 1) { ?>
+    <p><a href="index.php?controller=repliques&action=delete&idReplique=<?php echo $urlPrimary ;?>">X</a> Réplique d'identifiant <a href="index.php?controller=repliques&action=read&idReplique=<?php echo $urlPrimary ;?>"><?php echo $htmlPrimary; ?> </a> </p>
+<?php } else { ?>
+        <p> Réplique de : <?php echo $t->get('nomReplique'); ?>
+        <a href="index.php?controller=repliques&action=read&idReplique=<?php echo $urlPrimary ;?>">Voir l'article </a>
+        </p>
+    <?php } ?>
+<?php } ?>
