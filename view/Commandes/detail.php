450316e3a4fc2fa80ecefc3a71aba88fbd6d159a
Jonathan-Besse <78204251+Jonathan-Besse@users.noreply.github.com>
2021-12-08 22:24:49 +0100

    Affichage commande

--- view/Commandes/detail.php
+++ view/Commandes/detail.php
@@ -1,9 +1,22 @@
 <?php
-echo htmlspecialchars($u->get('codeClient'));
-echo htmlspecialchars(' '.$u->get('nomClient'));
-echo htmlspecialchars(' '.$u->get('prenomClient').' ');
-echo ('<a href="?controller=clients&action=delete&codeClient='.$u->get('codeClient').'"     >Supprimer</a>');
+echo 'Id de la commande : ' . htmlspecialchars($c->get('codeCommande'));
 echo '<br>';
-echo ('<a href="?controller=clients&action=update&codeClient='.$u->get('codeClient').'"     >Modifier</a>');
+echo 'Date de la commande : ' . htmlspecialchars(' '.$c->get('dateCommande'));
+echo '<br>';
+echo 'Code client : ' . htmlspecialchars(' '.$c->get('codeClient').' ');
+echo '<br>';
+echo '<br>';
+echo 'contenu de la commande : ';
+echo '<br>';
+echo '<br>';
+foreach ($c->get('idReplique_qte') as $item) {
+    foreach ($item as $key => $value){
+
+        if ($key == 'qte') { echo 'Quantité : ';}else{echo 'Id Replique : ';}
+        echo htmlspecialchars(' ' . $value . ' ');
+        echo '<br>';
+    }
+}
+echo ('<a href="?controller=clients&action=delete&codeClient='.$c->get('codeCommande').'"     >Supprimer</a>');
 echo '<br>';
 ?>
